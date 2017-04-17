8e00818e450bd986ca9b266de82542dec139107c
SKalt <kalt.steven@gmail.com>
2017-04-17 19:17:06 -0400

    refactored inputs, corrected gmlId array input for multi-*.

--- geomToGml3.js
+++ geomToGml3.js
@@ -17,49 +17,57 @@ const capital = (str) => str.replace(/^./, (letter) => letter.toUpperCase());
 const lower = (str) => str.replace(/^./, (letter) => letter.toLowerCase());
 
 var converter = {
-    '_multi': function(name, memberName, geom, gmlId, srsName, srsDimension=''){
+    '_multi': function(name, memberName, geom, gmlId, params={}){
+	var {srsName:srsName, srsDimension:srsDimension, gmlIds:gmlIds} = params;
 	let multi = `<gml:${name}${attrs({srsName, 'gml:id':gmlId})}>`;
-	for (let member of geom){
-	    var memberType;
+	for (var i = 0; i < geom.length; i ++){
+	    var member = geom[i];
+	    var memberType, _gmlId;
 	    if (name == 'MultiGeometry'){
+		_gmlId = member.id || (gmlIds || [])[i] || '';
 		memberType = member.type;
 		member = member.coordinates;
 	    } else {
-		 memberType = {'MultiPoint':'Point',
-			       'MultiCurve':'LineString',
-			       'MultiSurface':'Polygon'}[name];
+		_gmlId = (gmlIds || [])[i] || '';
+		memberType = {'MultiPoint':'Point',
+			      'MultiCurve':'LineString',
+			      'MultiSurface':'Polygon'}[name];
 	    }
 	    if (!this[memberType]){throw new Error(`memberType:${memberType}`);}
 	    multi += `<gml:${memberName}>` +
-		        this[memberType](member, srsName='') +
+		this[memberType](member, _gmlId, params) +
 		     `</gml:${memberName}>`;
 	}
 	multi += `</gml:${name}>`;
 	return multi;
     },
-    'Point': function(coords, gmlId, srsName, srsDimension){
+    'Point': function(coords, gmlId, params={}){
+	var {srsName:srsName, srsDimension:srsDimension} = params;
 	return `<gml:Point${attrs({srsName:srsName, 'gml:id': gmlId})}>` +
 	         `<gml:pos${attrs({srsDimension})}>` +
-	               coords.join() +
+	               coords.join(' ') +
 	          '</gml:pos>' +
 	        '</gml:Point>';
     },
-    'LineString': function(coords, gmlId, srsName, srsDimension){
+    'LineString': function(coords, gmlId, params={}){
+	var {srsName:srsName, srsDimension:srsDimension} = params;
 	return `<gml:LineString${attrs({srsName, 'gml:id':gmlId})}>` +
 	    `<gml:posList${attrs({srsDimension})}>` +
-	              coords.join(' ') +
+	              coords.map((e)=>e.join(' ')).join(' ') + 
 	          '</gml:posList>' +
 	       '</gml:LineString>';
     },
-    'LinearRing': function(coords, gmlId, srsName, srsDimension){
+    'LinearRing': function(coords, gmlId, params={}){
+	var {srsName:srsName, srsDimension:srsDimension} = params;
 	return `<gml:LinearRing${attrs({'gml:id':gmlId, srsName})}>` +
 	          `<gml:posList${attrs({srsDimension})}>` +
-	            coords.join(' ') + 
+	             coords.map((e)=>e.join(' ')).join(' ') + 
 	          '</gml:posList>' + 
 	       '</gml:LinearRing>';
     },
-    'Polygon': function(coords, gmlId, srsName, srsDimension){
+    'Polygon': function(coords, gmlId, params={}){
 	// geom.coordinates are arrays of LinearRings
+	var {srsName:srsName, srsDimension:srsDimension} = params;
 	let polygon = `<gml:Polygon${attrs({srsName, 'gml:id':gmlId})}>` +
 		       '<gml:exterior>' +
 		          this.LinearRing(coords[0]) +
@@ -75,33 +83,32 @@ var converter = {
 	return polygon;
     },
     
-    'MultiPoint': function(coords, gmlId, srsName, srsDimension){
-	return this._multi('MultiPoint', 'pointMember',
-			   coords, gmlId, srsName, srsDimension);
+    'MultiPoint': function(coords, gmlId, params={}){
+	return this._multi('MultiPoint', 'pointMember', coords, gmlId, params);
     },
-    'MultiLineString': function(coords, gmlId, srsName, srsDimension){
-	return this._multi('MultiCurve', 'curveMember',
-	    coords, gmlId, srsName, srsDimension);
+    'MultiLineString': function(coords, gmlId, params={}){
+	return this._multi('MultiCurve', 'curveMember', coords, gmlId, params);
     },
-    'MultiPolygon': function(coords, gmlId, srsName, srsDimension){
+    'MultiPolygon': function(coords, gmlId, params={}){
 	return this._multi('MultiSurface', 'surfaceMember',
-	    coords, gmlId, srsName, srsDimension);
+			   coords, gmlId, params);
     },
-    'GeometryCollection': function(geoms, gmlId, srsName, srsDimension){
+    'GeometryCollection': function(geoms, gmlId, params={}){
 	return this._multi('MultiGeometry', 'geometryMember',
-			   geoms, gmlId, srsName, srsDimension);
+			   geoms, gmlId, params);
     }
 };
 
 function geomToGml(geom,
 		   gmlId,
 		   srsName='http://www.opengis.net/def/crs/EPSG/0/4326',
-		   srsDimension){
-    return converter[geom.type](
-	geom.coordinates || geom.geometries,
-	gmlId,
-	srsName,
-	srsDimension
-    ); 
+		   srsDimension,
+		   gmlIds=[]){
+    return converter[geom.type](geom.coordinates || geom.geometries,
+				gmlId,
+				srsName,
+				srsDimension,
+				gmlIds
+			       ); 
 }
 exports.geomToGml = geomToGml;
